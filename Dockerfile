a04edae24404f53c88e4008cab84c8a3d7fdd1ff
Phillip Knezevich <phknezev@redhat.com>
2024-05-22 20:12:18 -0400

    simplified CE receiver

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,7 @@ RUN npm install
 EXPOSE 8080
 CMD ["node","index.js"]
 
-#docker run --rm  --name nodejs-cloud -p 8080:8080 -it --entrypoint bash quay.io/pknezevich/node-cloudevent
+#docker run --rm  --name nodejs-cloud -p 8080:8080 quay.io/pknezevich/node-cloudevent
+
+
+
--- index.js
+++ index.js
@@ -1,13 +1,40 @@
-const app = require("express")();
+
+const app = require('express')();
 const { HTTP } = require("cloudevents");
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
 
-app.post("/", (req, res) => {
-  // body and headers come from an incoming HTTP request, e.g. express.js
-  const receivedEvent = HTTP.toEvent({ headers: req.headers, body: req.body });
-  console.log(receivedEvent);
+app.post('/', (req, res) => {
+  try {
+    const receivedEvent = HTTP.toEvent({ headers: req.headers, body: req.body });
+    console.log('CloudEvent Object received. \n');
+    console.log('Version: ', receivedEvent.specversion, ' \n');
+    console.log('Type: ', receivedEvent.type, ' \n');
+    console.log('Data: ', receivedEvent.data, ' \n');
+    res.status(201).send("Event Accepted");
+
+  } catch(err) {
+    console.error('Error', err);
+    res.status(415)
+          .header("Content-Type", "application/json")
+          .send(JSON.stringify(err));
+  }
 });
-
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-	console.log('App version 1.0 listening on: ', port);
+  console.log('App Version 1.0 listening on: ', port);
 });
+
+// const app = require("express")();
+// const { HTTP } = require("cloudevents");
+
+// app.post("/", (req, res) => {
+//   // body and headers come from an incoming HTTP request, e.g. express.js
+//   const receivedEvent = HTTP.toEvent({ headers: req.headers, body: req.body });
+//   console.log(receivedEvent);
+// });
+
+// const port = process.env.PORT || 8080;
+// app.listen(port, () => {
+// 	console.log('App version 1.0 listening on: ', port);
+// });
